f24452aedd851b0d0ce30cc90bfd2cc20bca2ff1
Nícolas <nicolasanunes@hotmail.com>
2025-11-03 15:23:12 -0300

    feat: shadcn-vue added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
-  <p class="text-3xl font-bold underline text-blue-500">Just testing if TailwindCSS is working!</p>
+  <p class="text-3xl font-bold underline text-blue-500">A button bellow to test shadcn-vue:</p>
+
+  <Button class="" variant="outline">Click me</Button>
 </template>
 
 <style scoped></style>
