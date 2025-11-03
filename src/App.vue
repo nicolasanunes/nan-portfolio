e34774a0d325fb1d49e093299572a9493d363b77
Nícolas <nicolasanunes@hotmail.com>
2025-11-03 14:53:45 -0300

    feat: tailwindCSS added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <p class="text-3xl font-bold underline text-blue-500">Just testing if TailwindCSS is working!</p>
 </template>
 
 <style scoped></style>
