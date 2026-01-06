abeceebd59647c6a7094ea3f0b1ac0a3c2a8ef91
Nícolas <nicolasanunes@hotmail.com>
2026-01-06 15:02:40 -0300

    feat: gh-pages implemented

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ const getServerConfig = () => {
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nan-portfolio/',
   server: getServerConfig(),
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
